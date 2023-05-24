63a5ff78405542f821c4546ba1c7632d94bfbb47
markovojinovic <77206213+markovojinovic@users.noreply.github.com>
2023-05-24 14:44:07 +0200

    Napravljen sablon za sintaksnu analizu

---
+++ src/rs/ac/bg/etf/pp1/sym_old.java
@@ -0,0 +1,47 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym_old {
+
+    // Keywords
+    public static final int PROG = 1;
+    public static final int PRINT = 2;
+    public static final int RETURN = 3;
+    public static final int READ = 4;
+    public static final int NEW = 5;
+
+    // Identifiers
+    public static final int DESIG = 6;
+    public static final int EXPR = 7;
+    public static final int TERM = 8;
+    public static final int FACTOR = 9;
+    public static final int IDENT = 10;
+
+    // Constants
+    public static final int NUMBER = 11;
+    public static final int CHAR = 12;
+    public static final int BOOL = 13;
+
+    // Operators
+    public static final int PLUS = 14;
+    public static final int PLUSPLUS = 15;
+    public static final int MINUS = 16;
+    public static final int MINUSMINUS = 17;
+    public static final int DOTCOMMA = 18;
+    public static final int COMMA = 19;
+    public static final int MUL = 20;
+    public static final int DIV = 22;
+    public static final int PERCENT = 22;
+    public static final int LPAREN = 23;
+    public static final int RPAREN = 24;
+    public static final int LBRACE = 25;
+    public static final int RBRACE = 26;
+    public static final int EQUAL = 27;
+    public static final int LSQUARE = 28;
+    public static final int RSQUARE = 29;
+    public static final int CHARCONST = 32;
+
+
+    public static final int EOF = 30;
+    public static final int VOID = 31;
+
+}
