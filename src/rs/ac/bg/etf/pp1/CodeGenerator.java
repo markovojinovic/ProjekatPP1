0f02bc2b72ac5ce14fe99db3617527260c940a96
markovojinovic <77206213+markovojinovic@users.noreply.github.com>
2023-06-27 13:08:42 +0200

    Ostale samo sitnice u CodeGenerator

--- src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -10,29 +10,31 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 // TODO: videti da li treba da se dodaje i dodavanje bool konstanti u tabelu simbola
-// TODO: idalje postoji bag kod linije niz[niz[jedan]] = niz[niz[0]] * 3; - MOZDA LINIJA 185
+
+class Expressions {
+    public int data;
+    public int type;
+    public static final int Constant = 0;
+    public static final int Variable = 1;
+}
 
 public class CodeGenerator extends VisitorAdaptor {
 
     private int mainPc;
     private String designatorName = "";
     private Queue<Integer>[] operations = new LinkedList[256];
-    private Queue<Integer>[] expressions = new LinkedList[256];
     private Queue<Integer> currentOperations;
-    private Queue<Integer> currentExpressions;
     private boolean arrayCreation = false, exprEnter = false, oneOperandDeref = false,
             matrixCreation = false, exprEnterMatrix = false;
-    private int indexing = 0, dereference = 0, matrixDerederence = 0;
+    private int dereference = 0, matrixDerederence = 0, indexing = 0;
     private Struct arrayType = null;
     private Obj arrayValue = null, arrayIndex = null;
 
     public CodeGenerator() {
         for (int i = 0; i < 256; i++) {
             operations[i] = new LinkedList<>();
-            expressions[i] = new LinkedList<>();
         }
         currentOperations = operations[indexing];
-        currentExpressions = expressions[indexing];
     }
 
     public int getMainPc() {
@@ -42,12 +44,16 @@ public class CodeGenerator extends VisitorAdaptor {
     private void afterStatementRestart() {
         arrayValue = null;
         arrayIndex = null;
+        indexing = 0;
+        dereference = 0;
     }
 
     public void visit(PrintStmt printStmt) {
         int kind = printStmt.getExpr().struct.getKind();
         if (kind == Struct.Array)
             kind = printStmt.getExpr().struct.getElemType().getKind();
+        if (kind == Struct.Array)
+            kind = printStmt.getExpr().struct.getElemType().getElemType().getKind();
         if (kind == Struct.Int) {
             Code.loadConst(5);
             Code.put(Code.print);
@@ -62,13 +68,9 @@ public class CodeGenerator extends VisitorAdaptor {
         Obj con = Tab.insert(Obj.Con, "$", factorNum.struct);
         con.setLevel(0);
         con.setAdr(factorNum.getValue());
-        if (indexing > 0) {
-            currentExpressions.add(con.getAdr());
-        } else {
-            Code.load(con);
-            if (!currentOperations.isEmpty()) {
-                Code.put(currentOperations.remove());
-            }
+        Code.load(con);
+        if (!currentOperations.isEmpty()) {
+            Code.put(currentOperations.remove());
         }
     }
 
@@ -110,9 +112,6 @@ public class CodeGenerator extends VisitorAdaptor {
     }
 
     public void visit(DesignatorStatementEqual designatorStatementEqual) {
-        if (designatorStatementEqual.getLine() == 20) {
-            boolean nesto = false;
-        }
         Obj index = Tab.insert(Obj.Var, "#index$", Tab.intType);
         if (arrayCreation) {
             arrayCreation = false;
@@ -124,107 +123,106 @@ public class CodeGenerator extends VisitorAdaptor {
             Code.store(designatorStatementEqual.getDesignator().obj);
         } else if (matrixCreation) {
             matrixCreation = false;
-            int iterations;
             // rowNumber columnNumber ---- stanje na steku u ovom trenutku
             Code.store(index);                                                 // broj kolona - duzina pojedinacnog niza
             Obj index2 = Tab.insert(Obj.Var, "#index2$", Tab.intType);
+            Obj curr = Tab.insert(Obj.Var, "#curr$", Tab.intType);
             Code.store(index2);                  // broj redova - duzina niza ciji je svaki elem niz duzine columnNumber
-            iterations = index2.getAdr();
-            Obj matrixAddress = Tab.insert(Obj.Var, "#matrix$", Tab.intType);
             Code.load(index2);
             Code.put(Code.newarray);
-            Code.store(matrixAddress);                                          // ne desava se ova linija u run
-            for (int i = 0; i < iterations; i++) {
-                Code.load(index);
-                Code.put(Code.newarray);
-                Obj arrayAddress = Tab.insert(Obj.Var, "#array$", Tab.intType);     // videti da li ovde treba da stoji ovaj tip
-                Code.store(arrayAddress);
-                Code.load(matrixAddress);
-                index2.setAdr(i);
-                Code.load(index2);
-                Code.load(arrayAddress);
-                Code.put(Code.astore);
-            }
-        } else if (dereference > 0) {
-            Obj arrayValue = Tab.insert(Obj.Var, "#value$", Tab.intType);           // videti da li ovde treba da stoji ovaj tip
-            Code.store(arrayValue);
-            Code.store(index);
-            Code.load(designatorStatementEqual.getDesignator().obj);
+            Code.loadConst(1);
+            Code.store(designatorStatementEqual.getDesignator().obj);                  // ne desava se ova linija u run
+
+            Code.loadConst(0);
+            Code.store(curr);
+            int pocetak = Code.pc;
+            Code.load(index2);
+            Code.load(curr);
+            Code.putFalseJump(Code.gt, 0);
+            int falseJump = Code.pc - 2;
             Code.load(index);
-            Code.load(arrayValue);
+            Code.put(Code.newarray);
+            if (designatorStatementEqual.getDesignator().obj.getType().getElemType().getElemType().getKind() == Struct.Int)
+                Code.loadConst(1);
+            else
+                Code.put(0);
+            Obj arrayAddress = Tab.insert(Obj.Var, "#array$", Tab.intType);
+            Code.store(arrayAddress);
+            Code.load(designatorStatementEqual.getDesignator().obj);
+            Code.load(curr);
+            Code.load(arrayAddress);
+            Code.put(Code.astore);
+            Code.load(curr);
+            Code.loadConst(1);
+            Code.put(Code.add);
+            Code.store(curr);
+            Code.putJump(pocetak);
+            Code.fixup(falseJump);
+        } else if (dereference > 0) { // index value
+            Code.load(designatorStatementEqual.getDesignator().obj);
+            // index value addr
+            Code.put(Code.dup_x2);
+            Code.put(Code.pop);
+            // addr index value
             Code.put(Code.astore);
             dereference--;
-        }else if(matrixDerederence > 0){
+        } else if (matrixDerederence > 0) { // row column value
             matrixDerederence--;
-            Obj matrixValue = Tab.insert(Obj.Var, "#value$", Tab.intType);
-            Code.store(matrixValue);
-            Obj columnIndex = Tab.insert(Obj.Var, "#index$", Tab.intType);
-            Obj rowIndex = Tab.insert(Obj.Var, "#index2$", Tab.intType);
-            Code.store(columnIndex);
-            Code.store(rowIndex);
+            Code.put(Code.dup_x2);
+            Code.put(Code.pop);
+            // value row column
+            Code.put(Code.dup_x2);
+            Code.put(Code.pop);
+            //column value row
             Code.load(designatorStatementEqual.getDesignator().obj);
-            Code.load(rowIndex);
+            // column value row addr
+            Code.put(Code.dup_x1);
+            Code.put(Code.pop);
+            // column value addr row
             Code.put(Code.aload);
-            Code.load(rowIndex);
-            Code.load(matrixValue);
+            // column value addrArray
+            Code.put(Code.dup_x2);
+            Code.put(Code.pop);
+            // addrArray column value
             Code.put(Code.astore);
-        }
-        else
+        } else
             Code.store(designatorStatementEqual.getDesignator().obj);
     }
 
     public void visit(DesignatorExpression designator) {
         designatorName = designator.getName();
         SyntaxNode parent = designator.getParent();
-        boolean pass = false;
         if ((DesignatorStatementPlusPlus.class != parent.getClass()
                 && DesignatorStatementMinusMinus.class != parent.getClass()) && oneOperandDeref) {
             oneOperandDeref = false;
 
         }
-        if (DesignatorStatementPlusPlus.class == parent.getClass()
-                || DesignatorStatementMinusMinus.class == parent.getClass())
-            pass = true;
         if (DesignatorStatementEqual.class != parent.getClass()) {
             if (dereference > 0 && exprEnter) {
-                Obj index = Tab.insert(Obj.Var, "#index$", Tab.intType);
-                Code.store(index);
-                if (pass) {
-                    arrayIndex = Tab.insert(Obj.Var, "#index$", Tab.intType);
-                    arrayIndex.setAdr(index.getAdr());
-                }
                 Code.load(designator.obj);
-                Code.load(index);
+                Code.put(Code.dup_x1);
+                Code.put(Code.pop);
                 Code.put(Code.aload);
-                if (indexing > 0) {
-                    Obj curr = Tab.insert(Obj.Var, "$$", Tab.intType);      // ovde su problemi - OBAVEZNO PROVERITI
-                    Code.store(curr);
-                    currentExpressions.add(curr.getAdr());
-                } else if (!currentOperations.isEmpty())
+                if (!currentOperations.isEmpty()) {
                     Code.put(currentOperations.remove());
+                }
                 dereference--;
             } else if (matrixDerederence > 0 && exprEnterMatrix) {      // rowIndex columnIndex -- sadrzaj steka
-                Obj columnIndex = Tab.insert(Obj.Var, "#index$", Tab.intType);
-                Obj rowIndex = Tab.insert(Obj.Var, "#index2$", Tab.intType);
-                Code.store(columnIndex);
-                Code.store(rowIndex);
+                Code.put(Code.dup_x1);
+                Code.put(Code.pop);
                 Code.load(designator.obj);
-                Code.load(rowIndex);
+                Code.put(Code.dup_x1);
+                Code.put(Code.pop);
                 Code.put(Code.aload);
-                Code.load(columnIndex);
+                Code.put(Code.dup_x1);
+                Code.put(Code.pop);
                 Code.put(Code.aload);
-                if (indexing > 0) {
-                    Obj curr = Tab.insert(Obj.Var, "$$", Tab.intType);      // ovde su problemi - OBAVEZNO PROVERITI
-                    Code.store(curr);
-                    currentExpressions.add(curr.getAdr());
-                } else if (!currentOperations.isEmpty())
+                if (!currentOperations.isEmpty()) {
                     Code.put(currentOperations.remove());
+                }
                 matrixDerederence--;
             } else {
-                if (indexing > 0)
-                    currentExpressions.add(designator.obj.getAdr());
-                else
-                    Code.load(designator.obj);
+                Code.load(designator.obj);
             }
         }
         exprEnter = false;
@@ -251,10 +249,6 @@ public class CodeGenerator extends VisitorAdaptor {
         currentOperations.add(Code.rem);
     }
 
-    public void visit(FactorExpr factorExpr) {
-        // TODO: dodati kod ovde za obradu izraza sa zagradama
-    }
-
     public void visit(StatementRead statementRead) {
         Code.put(Code.read);
         Code.store(statementRead.getDesignator().obj);
@@ -262,6 +256,7 @@ public class CodeGenerator extends VisitorAdaptor {
     }
 
     public void visit(DesignatorStatementPlusPlus designatorStatementPlusPlus) {
+        // TODO: prepraviti ovo
         Obj con = Tab.insert(Obj.Con, "$", Tab.intType);
         con.setLevel(0);
         con.setAdr(1);
@@ -282,6 +277,7 @@ public class CodeGenerator extends VisitorAdaptor {
     }
 
     public void visit(DesignatorStatementMinusMinus designatorStatementMinusMinus) {
+        // TODO: prepraviti ovo
         Obj con = Tab.insert(Obj.Con, "$", Tab.intType);
         con.setLevel(0);
         con.setAdr(1);
@@ -310,29 +306,6 @@ public class CodeGenerator extends VisitorAdaptor {
         arrayType = factorNewExpr.getType().struct;
     }
 
-    public void visit(LsquareClass lsquareClass) {
-        indexing++;
-        currentOperations = operations[indexing];
-        currentExpressions = expressions[indexing];
-    }
-
-    public void visit(RsquareClass rsquareClass) {
-        if (currentExpressions.isEmpty())
-            return;
-        Obj con = Tab.insert(Obj.Con, "$", Tab.intType);
-        con.setAdr(currentExpressions.remove());
-        Code.load(con);
-        while (!currentExpressions.isEmpty() && !currentOperations.isEmpty()) {
-            con = Tab.insert(Obj.Con, "$", Tab.intType);
-            con.setAdr(currentExpressions.remove());
-            Code.load(con);
-            Code.put(currentOperations.remove());
-        }
-        indexing--;
-        currentOperations = operations[indexing];
-        currentExpressions = expressions[indexing];
-    }
-
     public void visit(ExprListExpr exprListExpr) {
         dereference++;
         exprEnter = true;
@@ -342,7 +315,6 @@ public class CodeGenerator extends VisitorAdaptor {
     public void visit(ExprListExprMatrix exprListExprMatrix) {
         matrixDerederence++;
         exprEnterMatrix = true;
-        // TODO: proveriti za flag-ove kao kod f-je iznad, treba li sta da se dodaje
     }
 
     public void visit(FactorNewTypeExpr factorNewTypeExpr) {
@@ -352,6 +324,16 @@ public class CodeGenerator extends VisitorAdaptor {
 
     public void visit(StatementDesignator statementDesignator) {
         afterStatementRestart();
+    }
+
+    public void visit(LsquareClass lsquareClass){
+        indexing++;
+        currentOperations = operations[indexing];
+    }
+
+    public void visit(RsquareClass rsquareClass){
+        indexing--;
+        currentOperations = operations[indexing];
     }
 
 }
--- src/rs/ac/bg/etf/pp1/ast/StatementDesignator.java
+++ src/rs/ac/bg/etf/pp1/ast/StatementDesignator.java
@@ -1,6 +1,6 @@
 // generated with ast extension for cup
 // version 0.8
-// 25/5/2023 22:47:49
+// 27/5/2023 13:7:33
 
 
 package rs.ac.bg.etf.pp1.ast;
